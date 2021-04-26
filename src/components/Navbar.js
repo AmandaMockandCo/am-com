d534d311ccd8d02ad2f65bb320358ca580e99159
amandarmock <82057977+amandarmock@users.noreply.github.com>
2021-04-26 14:58:21 -0700

    header style finally!

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -14,36 +14,38 @@ function Navbar() {
           <nav className="navbar">
               <div className="navbar-container">
                 <div className="logo">Amanda Mock</div>
-                 <div className='menu-icon' onClick={ handleClick }>
-                     <i className={ click ? 'fas fa-times' : 'fas fa-bars' } />
-                 </div>
-                 <ul className={click ? 'nav-menu active' : 'nav-menu'}>
-                    <li className='nav-item'>
-                        <Link to='/' className='nav-links' onClick={closeMobileMenu}>
-                        About     
-                        </Link> 
-                    </li>
-                    <li className='nav-item'>
-                        <Link to='/' className='nav-links' onClick={closeMobileMenu}>
-                        Approach    
-                        </Link> 
-                    </li>
-                    <li className='nav-item'>
-                        <Link to='/' className='nav-links' onClick={closeMobileMenu}>
-                        Projects     
-                        </Link> 
-                    </li>
-                    <li className='nav-item'>
-                        <Link to='/' className='nav-links' onClick={closeMobileMenu}>
-                        Work With Me     
-                        </Link> 
-                    </li>
-                    <li className='nav-item'>
-                        <Link to='/' className='nav-links' onClick={closeMobileMenu}>
-                        Contact     
-                        </Link> 
-                    </li>
-                 </ul>
+                    <div className="menu-container">
+                        <div className='menu-icon' onClick={ handleClick }>
+                            <i className={ click ? 'fas fa-times' : 'fas fa-bars' } />
+                        </div>
+                        <ul className={click ? 'nav-menu active' : 'nav-menu'}>
+                            <li className='nav-item'>
+                                <Link to='/' className='nav-links' onClick={closeMobileMenu}>
+                                About     
+                                </Link> 
+                            </li>
+                            <li className='nav-item'>
+                                <Link to='/' className='nav-links' onClick={closeMobileMenu}>
+                                Approach    
+                                </Link> 
+                            </li>
+                            <li className='nav-item'>
+                                <Link to='/' className='nav-links' onClick={closeMobileMenu}>
+                                Projects     
+                                </Link> 
+                            </li>
+                            <li className='nav-item'>
+                                <Link to='/' className='nav-links' onClick={closeMobileMenu}>
+                                Work With Me     
+                                </Link> 
+                            </li>
+                            <li className='nav-item'>
+                                <Link to='/' className='nav-links' onClick={closeMobileMenu}>
+                                Contact     
+                                </Link> 
+                            </li>
+                        </ul>
+                    </div>
               </div>
           </nav>
         </>
